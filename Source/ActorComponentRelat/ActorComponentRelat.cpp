63aa41470d4b9400dc6a7d8a7a35e92d98509a66
ALTimeCPP <110627536+ALTimeCPP@users.noreply.github.com>
2022-11-05 21:54:12 +0400

    ActorComponentRelat
    Initial Commit shell project

---
+++ Source/ActorComponentRelat/ActorComponentRelat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActorComponentRelat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActorComponentRelat, "ActorComponentRelat" );
